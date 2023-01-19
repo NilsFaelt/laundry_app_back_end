cd10e867ec47bc95e05fa1b0663d7dd789d07d9f
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-01-19 12:44:41 +0100

    fixed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
